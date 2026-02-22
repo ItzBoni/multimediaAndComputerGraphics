42dd55a15eec5ab3527ed585e971b2dc99bd4289
Santiago Bonilla <112128287+ItzBoni@users.noreply.github.com>
2026-02-21 18:16:09 -0600

    First_Midterm: invertColors is done. Starting with declarations in controllers.

--- First_Midterm/src/main/java/com/javafxdemo/first_midterm/utils/ImageTransformer.java
+++ First_Midterm/src/main/java/com/javafxdemo/first_midterm/utils/ImageTransformer.java
@@ -3,7 +3,7 @@ package src.main.java.com.javafxdemo.first_midterm.utils;
 import java.awt.image.BufferedImage;
 
 public interface ImageTransformer {
-    public BufferedImage invertColors(BufferedImage image);
-    public BufferedImage cut(BufferedImage image);
-    public BufferedImage rotate(BufferedImage image);
+    public void invertColors();
+    public void cut();
+    public void rotate();
 }
--- First_Midterm/src/main/java/com/javafxdemo/first_midterm/utils/ImageTransformerTool.java
+++ First_Midterm/src/main/java/com/javafxdemo/first_midterm/utils/ImageTransformerTool.java
@@ -1,4 +1,55 @@
 package src.main.java.com.javafxdemo.first_midterm.utils;
 
-public class ImageTransformerTool {
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.image.BufferedImage;
+
+public class ImageTransformerTool implements ImageTransformer{
+    //Saves the edits into a new BufferedImage so the user can keep editing
+    private BufferedImage resultImage;
+
+    //Constructor and setter(s)
+    public ImageTransformerTool(@NotNull BufferedImage image){
+        setResultImage(new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB));
+        java.awt.Graphics g = this.resultImage.getGraphics();
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+    }
+
+    private void setResultImage(BufferedImage image){
+        this.resultImage = image;
+    }
+
+    //Overrides methods from ImageTransformer. Better code quality here :3
+    @Override
+    public void invertColors(){
+        for (int x  = 0; x < this.resultImage.getWidth(); x++){
+            for (int y = 0; y < this.resultImage.getHeight(); y++){
+                //Separate every color channel of the pixel
+                int rgba = this.resultImage.getRGB(x, y);
+                int a = (rgba >> 24) & 0xFF;
+                int r = (rgba >> 16) & 0xFF;
+                int g = (rgba >> 8) & 0xFF;
+                int b = rgba & 0xFF;
+
+                //Math to invert the color of the image
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+
+                // Recompose the pixel
+                int invertedRgba = (a << 24) | (r << 16) | (g << 8) | b;
+                this.resultImage.setRGB(x, y, invertedRgba);
+            }
+        }
+    }
+
+    @Override
+    public void cut(){
+    }
+
+    @Override
+    public void rotate(){
+
+    }
 }
